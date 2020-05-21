4a0169fabbad7b3f8fcda9b4725afc763fd76c03
pcr <rongassride@gmail.com>
2020-05-21 09:57:30 +0900

    script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,9 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot | grep jar | awk '{print $1}')
 
-echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -21,7 +21,7 @@ else
     sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
